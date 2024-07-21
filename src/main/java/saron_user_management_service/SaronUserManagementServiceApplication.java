d2e758f32792949657f95752589d983484714916
sarontadesse <sarontadesse456@gmail.com>
2024-07-20 22:46:59 -0500

    initial commit

---
+++ src/main/java/saron_user_management_service/SaronUserManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package saron_user_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaronUserManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaronUserManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/saron_user_management_service/SaronUserManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package saron_user_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaronUserManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
